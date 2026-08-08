9ad965988cb81825bd1eb7b84cbced6548c20f4c
its-Sudhanshu <131343415+its-Sudhanshu@users.noreply.github.com>
2026-08-09 02:02:32 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,11 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+//IIFE is use for removing the pollution of global scope( means those globle variables who creates problem in function exicuton)    
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();  // here seicolon is imp for exicution of both function
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
